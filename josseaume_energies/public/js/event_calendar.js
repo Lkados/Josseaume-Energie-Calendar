a7090b4be1ea3be969157a1d6fb6fb5d69ef6070
Mohamed <mokachtit@gmail.com>
2025-05-16 21:33:39 +0200

    added event calendar

--- josseaume_energies/public/js/event_calendar.js
+++ josseaume_energies/public/js/event_calendar.js
@@ -1,13 +1,3 @@
-frappe.ui.form.on("Event", {
-	onload_post_render: function (frm) {
-		// Définir la vue par défaut sur Calendrier
-		if (frappe.route_options && !frappe.route_options.view) {
-			frappe.set_route("List", "Event", "Calendar");
-		}
-	},
-});
-
-// Personnaliser la vue Calendrier
 frappe.views.calendar["Event"] = {
 	field_map: {
 		start: "starts_on",
@@ -19,26 +9,68 @@ frappe.views.calendar["Event"] = {
 		color: "color",
 	},
 	options: {
-		initialView: "timeGridDay", // Vue jour par défaut dans le calendrier
-		slotMinTime: "06:00:00", // Début à 6h
-		slotMaxTime: "20:00:00", // Fin à 20h
-		slotDuration: "01:00:00", // Créneaux d'une heure (plus naturel)
+		initialView: "timeGridDay",
+		slotMinTime: "06:00:00",
+		slotMaxTime: "20:00:00",
+		// Diviser la journée en deux parties seulement (matin et après-midi)
+		slotDuration: "07:00:00",
+		headerToolbar: {
+			left: "prev,next today",
+			center: "title",
+			right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+		},
+		allDaySlot: true,
 		views: {
 			timeGridDay: {
-				type: "timeGrid",
-				duration: { days: 1 },
-				// Division en deux sections
-				slotDuration: "06:00:00", // Chaque section dure 6h
-				slotLabelContent: function (arg) {
-					var hour = arg.date.getHours();
+				// Configurer la vue jour avec seulement deux créneaux
+				slotLabelFormat: function (info) {
+					// Ne pas utiliser slotLabelContent car il peut causer des problèmes avec l'UI
+					const hour = info.date.getHours();
 					if (hour >= 6 && hour < 13) {
-						return { html: "<strong>Matin</strong>" };
+						return "Matin";
 					} else if (hour >= 13 && hour < 20) {
-						return { html: "<strong>Après-midi</strong>" };
+						return "Après-midi";
 					}
+					return "";
 				},
 			},
 		},
 	},
 	get_events_method: "frappe.desk.doctype.event.event.get_events",
 };
+
+// Assurez-vous que la vue Calendrier est définie par défaut
+frappe.listview_settings["Event"] = frappe.listview_settings["Event"] || {};
+frappe.listview_settings["Event"].onload = function (listview) {
+	if (!frappe.route_options || !frappe.route_options.view) {
+		frappe.route_options = frappe.route_options || {};
+		frappe.route_options.view = "Calendar";
+	}
+};
+
+// Préserver la fonctionnalité d'ajout d'événement
+$(document).on("click", ".fc-timeGridDay-view .fc-timegrid-slot", function (e) {
+	if (!frappe.views.calendar["Event"].calendar) return;
+
+	const calendar = frappe.views.calendar["Event"].calendar;
+	const event = calendar.getEvents();
+	const date = calendar.getDate();
+
+	// Obtenir le moment du clic
+	const slot = $(e.target).closest(".fc-timegrid-slot");
+	const hour = parseInt(slot.attr("data-time").split(":")[0]) || 8;
+
+	// Créer un nouvel événement
+	frappe.new_doc("Event", {
+		starts_on:
+			frappe.datetime.add_days(frappe.datetime.nowdate(), date.getDay() - moment().day()) +
+			" " +
+			hour +
+			":00:00",
+		ends_on:
+			frappe.datetime.add_days(frappe.datetime.nowdate(), date.getDay() - moment().day()) +
+			" " +
+			(hour + 1) +
+			":00:00",
+	});
+});
