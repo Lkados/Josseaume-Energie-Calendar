6314b178c5581481de2dc27e78efe017383dcf73
Mohamed <mokachtit@gmail.com>
2025-05-16 23:32:07 +0200

    added event calendar

--- josseaume_energies/hooks.py
+++ josseaume_energies/hooks.py
@@ -1,16 +1,16 @@
 # josseaume_energie_calendar/hooks.py
 
-app_name = "josseaume_energie_calendar"
-app_title = "Josseaume Energie Calendar"
-app_publisher = "Josseaume"
+app_name = "josseaume_energie"
+app_title = "Josseaume Energie"
+app_publisher = "Mohamed Kachtit"
 app_description = "Personnalisation du calendrier Event (vue jour bi-colonne)"
 app_icon = "octicon octicon-calendar"
 app_color = "grey"
 app_version = "0.1.0"
 
 # Inclure notre JS/CSS personnalisé dans le Desk
-app_include_js = ["/assets/josseaume_energie_calendar/js/event_calendar.js"]
-app_include_css = ["/assets/josseaume_energie_calendar/css/event_calendar.css"]
+app_include_js = ["/assets/josseaume_energie/js/event_calendar.js"]
+app_include_css = ["/assets/josseaume_energie/css/event_calendar.css"]
 
 # Optionnel : utiliser le hook doctype_calendar_js pour cibler spécifiquement Event
 doctype_calendar_js = {
