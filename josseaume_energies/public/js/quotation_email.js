72565f9948e5b7a2cf353da6da4dab1d80be7e70
Lkados <mokachtit@gmail.com>
2025-09-08 14:38:49 +0200

    added email template

--- josseaume_energies/public/js/quotation_email.js
+++ josseaume_energies/public/js/quotation_email.js
@@ -2,54 +2,13 @@
 
 frappe.ui.form.on('Quotation', {
     refresh: function(frm) {
-        // Surcharger la fonction d'envoi d'email
-        if (!frm.custom_email_dialog_modified) {
-            const original_email_doc = frm.email_doc;
-            
-            frm.email_doc = function(message) {
-                // Appeler la fonction originale
-                original_email_doc.apply(frm, arguments);
-                
-                // Attendre que le dialogue soit créé
-                setTimeout(() => {
-                    const dialog = frappe.msgprint.dialog;
-                    if (dialog && dialog.fields_dict) {
-                        // Récupérer les valeurs par défaut depuis le backend
-                        frappe.call({
-                            method: 'josseaume_energies.quotation_email.get_email_defaults_for_quotation',
-                            args: {
-                                quotation: frm.doc.name
-                            },
-                            callback: function(r) {
-                                if (r.message) {
-                                    // Définir le sujet
-                                    if (dialog.fields_dict.subject) {
-                                        dialog.set_value('subject', r.message.subject);
-                                    }
-                                    
-                                    // Définir le message
-                                    if (dialog.fields_dict.message) {
-                                        dialog.set_value('message', r.message.message);
-                                    }
-                                }
-                            }
-                        });
-                    }
-                }, 100);
-            };
-            
-            frm.custom_email_dialog_modified = true;
-        }
-    }
-});
-
-// Alternative : Hook sur le bouton Email
-frappe.ui.form.on('Quotation', {
-    before_email: function(frm) {
-        // Cette fonction sera appelée avant l'ouverture du dialogue d'email
-        frm.email_defaults = {
-            subject: `Devis N° ${frm.doc.name} - JOSSEAUME ÉNERGIES`,
-            message: `Bonjour,
+        // Override the email button functionality
+        setTimeout(() => {
+            frm.page.btn_primary.off('click');
+            frm.page.btn_primary.on('click', function() {
+                if (frm.page.btn_primary.text() === __('Email') || frm.page.btn_primary.text() === 'Email') {
+                    // Préparer le message par défaut
+                    const default_message = `Bonjour,
 
 Veuillez trouver ci-joint le document : Devis N° ${frm.doc.name}.
 
@@ -64,7 +23,90 @@ JOSSEAUME ÉNERGIES
 contact@josseaume-energies.com
 www.josseaume-energies.com
 
-🌱 N'imprimer que si nécessaire.`
-        };
+🌱 N'imprimer que si nécessaire.`;
+
+                    // Ouvrir le dialogue d'email avec les valeurs par défaut
+                    new frappe.views.CommunicationComposer({
+                        doc: frm.doc,
+                        frm: frm,
+                        subject: `Devis N° ${frm.doc.name} - JOSSEAUME ÉNERGIES`,
+                        message: default_message,
+                        real_name: frappe.user_info().fullname,
+                        print_format: frm.meta.default_print_format || "Standard",
+                        select_print_format: true,
+                        send_email: true
+                    });
+                } else {
+                    // Si ce n'est pas le bouton Email, exécuter l'action par défaut
+                    frm.page.btn_primary.trigger('click');
+                }
+            });
+        }, 1000);
     }
 });
+
+// Hook plus robuste pour intercepter tous les dialogues d'email
+frappe.views.CommunicationComposer = class extends frappe.views.CommunicationComposer {
+    constructor(opts) {
+        // Si on est sur un devis et qu'il n'y a pas de message défini
+        if (opts.doc && opts.doc.doctype === 'Quotation' && !opts.message) {
+            opts.subject = opts.subject || `Devis N° ${opts.doc.name} - JOSSEAUME ÉNERGIES`;
+            opts.message = `Bonjour,
+
+Veuillez trouver ci-joint le document : Devis N° ${opts.doc.name}.
+
+Nous restons à votre disposition pour tous renseignements complémentaires.
+
+Cordialement,
+
+JOSSEAUME ÉNERGIES
+210 IMPASSE DES TUILERIES - LE VIVIER DANGER
+60650 ONS EN BRAY
+03 44 80 53 75
+contact@josseaume-energies.com
+www.josseaume-energies.com
+
+🌱 N'imprimer que si nécessaire.`;
+        }
+        super(opts);
+    }
+    
+    setup_fields() {
+        super.setup_fields();
+        
+        // Si on est sur un devis, s'assurer que les champs sont bien remplis
+        if (this.doc && this.doc.doctype === 'Quotation') {
+            setTimeout(() => {
+                if (this.dialog) {
+                    // Remplir le sujet
+                    if (this.dialog.fields_dict.subject && !this.dialog.fields_dict.subject.get_value()) {
+                        this.dialog.fields_dict.subject.set_value(`Devis N° ${this.doc.name} - JOSSEAUME ÉNERGIES`);
+                    }
+                    
+                    // Remplir le contenu/message
+                    const message_field = this.dialog.fields_dict.content || this.dialog.fields_dict.message || this.dialog.fields_dict.body;
+                    if (message_field && !message_field.get_value()) {
+                        const message = `Bonjour,
+
+Veuillez trouver ci-joint le document : Devis N° ${this.doc.name}.
+
+Nous restons à votre disposition pour tous renseignements complémentaires.
+
+Cordialement,
+
+JOSSEAUME ÉNERGIES
+210 IMPASSE DES TUILERIES - LE VIVIER DANGER
+60650 ONS EN BRAY
+03 44 80 53 75
+contact@josseaume-energies.com
+www.josseaume-energies.com
+
+🌱 N'imprimer que si nécessaire.`;
+                        
+                        message_field.set_value(message);
+                    }
+                }
+            }, 100);
+        }
+    }
+};
